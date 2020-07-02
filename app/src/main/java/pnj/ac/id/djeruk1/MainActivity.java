830c60e5d16579f8f5903baf58ec81a87a30869b
unknown <dendyfarez1@gmail.com>
2020-07-02 21:03:01 +0700

    Djeruk1

---
+++ app/src/main/java/pnj/ac/id/djeruk1/MainActivity.java
@@ -0,0 +1,55 @@
+package pnj.ac.id.djeruk1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class   MainActivity extends AppCompatActivity {
+    private Button btn_camera;
+    private ImageView ivCamera;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int CAMERA_REQUEST_CODE = 7777;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_camera = (Button) findViewById(R.id.btn_camera);
+        ivCamera = (ImageView) findViewById(R.id.iv_camera);
+
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //intent khusus untuk menangkap foto lewat kamera
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case(CAMERA_REQUEST_CODE) :
+                if(resultCode == Activity.RESULT_OK)
+                {
+                    // result code sama, save gambar ke bitmap
+                    Bitmap bitmap;
+                    bitmap = (Bitmap) data.getExtras().get("data");
+                    ivCamera.setImageBitmap(bitmap);
+                }
+                break;
+        }
+    }
+
+}
---
+++ app/src/main/java/pnj/ac/id/djeruk1/SplashScreen.java
@@ -0,0 +1,36 @@
+package pnj.ac.id.djeruk1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+    //Set waktu lama splashscreen
+    private static int splashInterval = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                //jeda selesai Splashscreen
+                this.finish();
+            }
+            private void finish() {
+                // TODO Auto-generated method stub
+
+            }
+        }, splashInterval);
+    }
+}
